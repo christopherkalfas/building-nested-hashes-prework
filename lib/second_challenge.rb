4ce4e51015e3a8d6b89476f077cf02688f297616
Christopher Kalfas <chriskalfas@yahoo.com>
2019-03-12 18:18:53 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,14 +2,20 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      :patriarch
-      :matriarch
+      :patriarch = {}
+      :matriarch 
       :hero 
       :hero_friends
       }, 
     :capulet => {
       #your key/value pairs here!
-      }}
+      :patriarch
+      :matriarch
+      :heroine
+      :heroine_friends
+      }
+    
+  }
 
   
 end
